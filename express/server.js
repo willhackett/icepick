2b5dbe1ecf4acc6447888a7eb093e12dfc5767fc
Matt Way <matt__way@hotmail.com>
2017-02-16 10:19:14 +1000

    multer and aws init

--- express/server.js
+++ express/server.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import multer from 'multer'
+import bodyParser from 'body-parser'
 import { config } from '/config/environment'
+import fileUpload from '/file/upload'
 
 const app = express()
+
+// middlewares
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// upload routes
+const upload = multer({ dest: 'temp_uploads/' })
+app.post('/upload/image/square', upload.single('square'), fileUpload)
+app.post('/upload/image/background', upload.single('background'), fileUpload)
+app.post('/upload/image/file', upload.single('file'), fileUpload)
 
 app.listen(config.PORT, function () {
   console.log(`Example app listening on port ${config.PORT}`)
