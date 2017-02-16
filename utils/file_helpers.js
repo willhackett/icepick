8cb311aa985c7c42dab7a71f3c17f76cb0e2986b
Matt Way <matt__way@hotmail.com>
2017-02-16 22:41:57 +1000

    file cleanup done

--- utils/file_helpers.js
+++ utils/file_helpers.js
@@ -1,5 +1,22 @@
+import fs from 'fs'
+import { config } from '/config/environment'
+
+const removeFile = path => {
+  fs.unlink(path, err => {
+    if(err) { console.log('Error deleting file:', path) }
+  })
+}
 
 const cleanupFiles = (req, res, next) => {
+
+  removeFile(req.file.path)
+  Object.keys(req.processed).forEach(key => {
+    const path = config.TEMP_UPLOAD_FOLDER + req.processed[key]
+    if(path !== req.file.path){
+      removeFile(path)
+    }
+  })
+
   return next()
 }
 
